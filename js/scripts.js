d5903e92b4b9c344ccbd621ae93552c56aa4465e
Bobby Martin and Cameron Button <cameron@>
2018-03-21 15:44:13 -0700

    assign values to the select button

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var userName = $("input#name").val();
+    var userAddress = $("input#address").val();
+    var userItem = $("#item").val();
+    // debugger;
+    $(".user-name").text(userName);
+    $(".user-address").text(userAddress);
+    $(".user-item").text(userItem);
+    event.preventDefault();
+  });
+});
